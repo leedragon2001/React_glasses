9402b2a9b17bef389d50818ebe01e60383703c98
LeeHuy <72535896+leedragon2001@users.noreply.github.com>
2023-06-26 11:17:56 +0700

    upload files

---
+++ src/Glasses/index.js
@@ -0,0 +1,90 @@
+import React, { Component } from "react";
+import data from "./dataGlasses.json";
+import { render } from "@testing-library/react";
+export default class Glasses extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listGlasses: data,
+      urlImg: "./img/v1.png",
+    };
+  }
+  handlChangeGlass(urlImg) {
+    this.setState({
+      urlImg,
+    });
+  }
+  renderListGlass() {
+    const { listGlasses } = this.state;
+    return listGlasses.map((glass) => {
+      return (
+        <>
+          <span key={glass.id}>
+            <img className="my-5 mx-4" src={glass.url} alt="hinh anh" />
+            <button
+              className="btn btn-warning"
+              onClick={() => {
+                this.handlChangeGlass(glass.url);
+              }}
+            >
+              Try it
+            </button>
+          </span>
+        </>
+      );
+    });
+  }
+  render() {
+    return (
+      <>
+        <header>
+          <div className="text-center bg-dark text-white py-4">
+            TRY GLASSES APP ONLINE
+          </div>
+        </header>
+        <div className="carousel row container-fluid">
+          <div className="col-sm-6 mt-5">
+            <div className="card card1 ">
+              <img
+                className="putGlass1"
+                src={this.state.urlImg}
+                alt="Anh minh hoa"
+              />
+              <img
+                className="card-img-top"
+                src="./img/model.jpg"
+                alt="hinh anh"
+              />
+              <div className="card-body ">
+                <h4 className="card-title">FENDI F8650</h4>
+                <p className="card-text">
+                  Light pink sqquare lenses define these sunglasses, ending with
+                  amother of pearl effect tip
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-6 mt-5">
+            <div className="card card2">
+              <img
+                className="putGlass2"
+                src={this.state.urlImg}
+                alt="Anh minh hoa"
+              />
+              <img
+                className="card-img-top"
+                src="./img/model.jpg"
+                alt="hinh anh"
+              />
+            </div>
+          </div>
+          <div className="col-sm-12 mt-5 ">
+            <div className="row card card3">
+              <div className="col-sm-12">{this.renderListGlass()}</div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
